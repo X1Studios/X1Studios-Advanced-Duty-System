df83692d50a4fceb6522f1326bdd282c0c14f9ed
Havoc Sheriff <sheriffx1studios@gmail.com>
2026-02-14 14:10:13 -0700

    Update client.lua

--- X1S-AdvDutySystem/client/client.lua
+++ X1S-AdvDutySystem/client/client.lua
@@ -1,77 +1,91 @@
 local onDuty = false
 local DutyBlips = {}
+local UpdateThread = nil
+
+-- ======================
+-- UTIL
+-- ======================
+
+local function clearAllBlips()
+    for _, data in pairs(DutyBlips) do
+        if data.blip then
+            RemoveBlip(data.blip)
+        end
+    end
+    DutyBlips = {}
+end
+
+local function stopUpdateThread()
+    if UpdateThread then
+        UpdateThread = nil
+    end
+end
 
 -- ======================
 -- DUTY MENU
 -- ======================
+
 RegisterCommand('dutymenu', function()
     if onDuty or IsNuiFocused() then return end
 
     SetNuiFocus(true, true)
 
     local departments = {}
-    for k, v in pairs(Config.Departments) do
-        departments[#departments + 1] = {
+    for k,v in pairs(Config.Departments) do
+        departments[#departments+1] = {
             value = k,
             label = v.label
         }
     end
 
     SendNUIMessage({
-        action = 'openDuty',
-        departments = departments
+        action='openDuty',
+        departments=departments
     })
 end)
 
 RegisterNUICallback('goOnDuty', function(data, cb)
     onDuty = true
     TriggerServerEvent('x1s-duty:onDuty', data)
-    SetNuiFocus(false, false)
     cb('ok')
 end)
 
 RegisterNUICallback('offDuty', function(_, cb)
-    onDuty = false
-    TriggerServerEvent('x1s-duty:offDuty')
-    SetNuiFocus(false, false)
-    cb('ok')
-end)
-
-RegisterNUICallback('close', function(_, cb)
-    SetNuiFocus(false, false)
+    goOffDuty()
     cb('ok')
 end)
 
 RegisterCommand('offduty', function()
-    if not onDuty then return end
+    if onDuty then goOffDuty() end
+end)
+
+function goOffDuty()
     onDuty = false
     TriggerServerEvent('x1s-duty:offDuty')
+
+    stopUpdateThread()
+    clearAllBlips()
+end
+
+RegisterNUICallback('close', function(_, cb)
+    SetNuiFocus(false,false)
+    cb('ok')
 end)
 
 -- ======================
 -- SUPERVISOR MENU
 -- ======================
+
 RegisterCommand('supervisormenu', function()
     TriggerServerEvent('x1s-duty:requestSupervisorMenu')
 end)
 
 RegisterNetEvent('x1s-duty:openSupervisor', function(data)
-    SetNuiFocus(true, true)
+    SetNuiFocus(true,true)
     SendNUIMessage({
-        action = 'openSupervisor',
-        players = data
+        action='openSupervisor',
+        players=data
     })
-end)
-
--- FORCED OFF EVENT
-RegisterNetEvent('x1s-duty:forcedOff', function()
-    onDuty = false
-
-    -- remove all blips when forced off
-    for id, blip in pairs(DutyBlips) do
-        RemoveBlip(blip)
-        DutyBlips[id] = nil
-    end
 end)
 
 RegisterNUICallback('forceOff', function(data, cb)
@@ -79,71 +93,76 @@ RegisterNUICallback('forceOff', function(data, cb)
     cb('ok')
 end)
 
--- ======================
--- BLIPS
--- ======================
-RegisterNetEvent('x1s-duty:addBlip', function(id, data)
-    CreateThread(function()
-        local tries = 0
-        local player = GetPlayerFromServerId(id)
-
-        while player == -1 and tries < 20 do
-            Wait(250)
-            player = GetPlayerFromServerId(id)
-            tries = tries + 1
-        end
-
-        if player == -1 then return end
-
-        local ped = GetPlayerPed(player)
-        if not DoesEntityExist(ped) then return end
-
-        if DutyBlips[id] then
-            RemoveBlip(DutyBlips[id])
-        end
-
-        local blip = AddBlipForEntity(ped)
-        SetBlipSprite(blip, data.sprite)
-        SetBlipColour(blip, data.color)
-        SetBlipScale(blip, 0.9)
-        SetBlipAsShortRange(blip, false)
-
-        BeginTextCommandSetBlipName('STRING')
-        AddTextComponentString(('[%s] %s'):format(data.callsign, data.name))
-        EndTextCommandSetBlipName(blip)
-
-        DutyBlips[id] = blip
-    end)
+RegisterNetEvent('x1s-duty:forcedOff', function()
+    goOffDuty()
 end)
 
-RegisterNetEvent('x1s-duty:removeBlip', function(id)
-    if DutyBlips[id] then
-        RemoveBlip(DutyBlips[id])
-        DutyBlips[id] = nil
+-- ======================
+-- BLIP SYNC
+-- ======================
+
+RegisterNetEvent('x1s-duty:syncBlips', function(players)
+
+    if not onDuty then return end
+
+    local myId = GetPlayerServerId(PlayerId())
+
+    clearAllBlips()
+
+    for serverId, data in pairs(players) do
+        if serverId ~= myId then
+
+            local blip = AddBlipForCoord(0.0,0.0,0.0)
+
+            SetBlipSprite(blip, data.sprite or 1)
+            SetBlipColour(blip, data.color or 0)
+            SetBlipScale(blip, 0.9)
+            SetBlipAsShortRange(blip, false)
+            ShowHeadingIndicatorOnBlip(blip, true)
+
+            BeginTextCommandSetBlipName('STRING')
+            AddTextComponentString(
+                ('[%s] %s'):format(data.callsign or "000", data.name or "Unknown")
+            )
+            EndTextCommandSetBlipName(blip)
+
+            DutyBlips[serverId] = {
+                blip = blip,
+                serverId = serverId
+            }
+        end
+    end
+
+    if not UpdateThread then
+        UpdateThread = CreateThread(function()
+            while onDuty do
+
+                for _, info in pairs(DutyBlips) do
+                    local player = GetPlayerFromServerId(info.serverId)
+
+                    if player ~= -1 then
+                        local ped = GetPlayerPed(player)
+
+                        if DoesEntityExist(ped) then
+                            local coords = GetEntityCoords(ped)
+                            SetBlipCoords(info.blip, coords.x, coords.y, coords.z)
+                        end
+                    end
+                end
+
+                Wait(500)
+            end
+
+            UpdateThread = nil
+        end)
     end
 end)
 
 -- ======================
 -- CLEANUP
 -- ======================
+
 AddEventHandler('onClientResourceStop', function(res)
     if res ~= GetCurrentResourceName() then return end
-    for _, blip in pairs(DutyBlips) do
-        RemoveBlip(blip)
-    end
-end)
-
-local DutyPlayers = {}
-
--- ======================
--- TOAST RECEIVER
--- ======================
-RegisterNetEvent('x1s-duty:toast', function(data)
-    if type(data) ~= 'table' then return end
-
-    SendNUIMessage({
-        action = 'toast',
-        message = data.message or '',
-        type = data.type or 'success'
-    })
+    clearAllBlips()
 end)
