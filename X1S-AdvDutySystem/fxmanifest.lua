7746abad6e75e0278050d8a8847bf3042c66d113
Havoc Sheriff <sheriffx1studios@gmail.com>
2026-02-20 15:25:51 -0700

    Update fxmanifest.lua

--- X1S-AdvDutySystem/fxmanifest.lua
+++ X1S-AdvDutySystem/fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'X1Studios'
 description 'Advanced Standalone Duty System'
-version '1.0.1'
+version '1.1.0'
 
 ui_page 'html/index.html'
 
@@ -25,4 +25,3 @@ client_scripts {
 server_scripts {
     'server/server.lua'
 }
-
